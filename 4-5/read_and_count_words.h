afbc38902b4f6da857a84fc6bbb5ba60fa8cb771
Yu Fu <yfu@yfu.me>
2014-08-03 16:15:47 -0400

    finished 4-5

---
+++ 4-5/read_and_count_words.h
@@ -0,0 +1,9 @@
+#ifndef GUARD_read_and_count_words_h
+#define GUARD_read_and_count_words_h
+
+#include <iostream>
+#include <vector>
+
+std::istream& read_and_count_words(std::istream& in, std::vector<std::string>& words);
+
+#endif
